65909e1b08795a81d0acb03dd71158a9442fcf84
NoMoreActimel <evolution3000futurama@gmail.com>
2023-10-16 23:20:56 +0300

    added cer and wer metrics calculation

--- hw_asr/metric/utils.py
+++ hw_asr/metric/utils.py
@@ -1,10 +1,10 @@
 # Don't forget to support cases when target_text == ''
-
-def calc_cer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
+import editdistance
 
 
-def calc_wer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
+def calc_cer(target_text: str, predicted_text: str) -> float:
+    return editdistance.eval(target_text, predicted_text) / len(target_text)
+
+def calc_wer(target_text: str, predicted_text: str) -> float:
+    target_splitted, predict_splitted = target_text.split(' '), predicted_text.split(' ')
+    return editdistance.eval(target_splitted, predict_splitted) / len(target_splitted)
