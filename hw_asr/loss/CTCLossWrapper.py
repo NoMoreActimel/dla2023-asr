e32de430d2f012e6aed4472edab43750425a1b00
NoMoreActimel <evolution3000futurama@gmail.com>
2023-10-17 01:40:04 +0300

    testing print

--- hw_asr/loss/CTCLossWrapper.py
+++ hw_asr/loss/CTCLossWrapper.py
@@ -7,6 +7,7 @@ class CTCLossWrapper(CTCLoss):
     def forward(self, log_probs, log_probs_length, text_encoded, text_encoded_length,
                 **batch) -> Tensor:
         log_probs_t = torch.transpose(log_probs, 0, 1)
+        print(f'{log_probs.shape}, {log_probs_t.shape}, {log_probs_length.shape}, {text_encoded.shape}, {text_encoded_length.shape}')
 
         return super().forward(
             log_probs=log_probs_t,
