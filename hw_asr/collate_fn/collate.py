67b0156afc6b3be41f453e577068a02e71685ec7
NoMoreActimel <evolution3000futurama@gmail.com>
2023-10-16 22:35:49 +0300

    added collate_fn

--- hw_asr/collate_fn/collate.py
+++ hw_asr/collate_fn/collate.py
@@ -1,4 +1,5 @@
 import logging
+import torch
 from typing import List
 
 logger = logging.getLogger(__name__)
@@ -8,7 +9,11 @@ def collate_fn(dataset_items: List[dict]):
     """
     Collate and pad fields in dataset items
     """
-
-    result_batch = {}
-    # TODO: your code here
-    raise NotImplementedError
+    # input fields: ["audio", "spectrogram", "duration","text", "text_encoded", "audio_path"]
+    # output fields: ["spectrogram", "text_encoded", "text_encoded_length", "text"]
+    return {
+        'spectrogram': torch.stack([row['spectrogram'] for row in dataset_items], axis=0),
+        'text_encoded': torch.stack([row['text_encoded'] for row in dataset_items], axis=0),
+        'text_encoded_length': torch.tensor([len(row['text']) for row in dataset_items], dtype=torch.int32),
+        'text': [row['text'] for row in dataset_items]
+    }
