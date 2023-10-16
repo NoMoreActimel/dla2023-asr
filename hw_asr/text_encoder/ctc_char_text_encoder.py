a7757bd9a453757fefbb85589eb0602b77c12082
NoMoreActimel <evolution3000futurama@gmail.com>
2023-10-16 20:05:36 +0300

    Added ctc_decode and ctc_beam_search implementations to ctc_char_text_encoder

--- hw_asr/text_encoder/ctc_char_text_encoder.py
+++ hw_asr/text_encoder/ctc_char_text_encoder.py
@@ -1,4 +1,5 @@
 from typing import List, NamedTuple
+from collections import defaultdict
 
 import torch
 
@@ -11,17 +12,45 @@ class Hypothesis(NamedTuple):
 
 
 class CTCCharTextEncoder(CharTextEncoder):
-    EMPTY_TOK = "^"
-
     def __init__(self, alphabet: List[str] = None):
         super().__init__(alphabet)
+        self.EMPTY_TOK = "^"
+        self.EMPTY_IND = 0
         vocab = [self.EMPTY_TOK] + list(self.alphabet)
         self.ind2char = dict(enumerate(vocab))
         self.char2ind = {v: k for k, v in self.ind2char.items()}
 
     def ctc_decode(self, inds: List[int]) -> str:
-        # TODO: your code here
-        raise NotImplementedError()
+        result = []
+        last_char = self.EMPTY_TOK
+
+        for ind in inds:
+            char = self.ind2char[ind]
+            if ind != self.EMPTY_IND and char != last_char:
+                result.append(char)
+            last_char = char
+
+        return result
+    
+    def _extend_and_merge(self, frame, state):
+        new_state = defaultdict(float)
+
+        for next_char_index, next_char_prob in enumerate(frame):
+            for (prefix, last_char), prefix_prob in state.items():
+                next_char = self.ind2char[next_char_index]
+
+                if next_char != last_char and next_char != self.EMPTY_TOK:
+                    # prefix is string -> immutable -> it's ok
+                    prefix += next_char
+
+                new_state[(prefix, next_char)] += prefix_prob * next_char_prob
+                last_char = next_char
+
+        return new_state
+    
+    def _truncate(self, state, beam_size):
+        sorted_stats_list = sorted(list(state.items()), key=lambda x: -x[1])
+        return dict(sorted_stats_list[:beam_size])
 
     def ctc_beam_search(self, probs: torch.tensor, probs_length,
                         beam_size: int = 100) -> List[Hypothesis]:
@@ -32,6 +61,15 @@ class CTCCharTextEncoder(CharTextEncoder):
         char_length, voc_size = probs.shape
         assert voc_size == len(self.ind2char)
         hypos: List[Hypothesis] = []
-        # TODO: your code here
-        raise NotImplementedError
+
+        # state[prefix, last_char] = prefix_prob
+        state = {('', self.EMPTY_TOK): 1.0}
+
+        for frame in probs:
+            state = self._extend_and_merge(frame, state)
+            state = self._truncate(state, beam_size)
+        
+        for (prefix, last_char), prefix_prob in state.items():
+            hypos.append(Hypothesis(text=prefix, prob=prefix_prob))
+        
         return sorted(hypos, key=lambda x: x.prob, reverse=True)
