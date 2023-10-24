2dd76f863ca47834bbb83e6599e801d2af33990b
NoMoreActimel <evolution3000futurama@gmail.com>
2023-10-24 20:52:33 +0300

    spec aug init fix

--- hw_asr/augmentations/spectrogram_augmentations/__init__.py
+++ hw_asr/augmentations/spectrogram_augmentations/__init__.py
@@ -1,1 +1,7 @@
+from hw_asr.augmentations.spectrogram_augmentations.FreqTimeMasking import FreqTimeMaskingSpecAug
+from hw_asr.augmentations.spectrogram_augmentations.TimeStretch import TimeStretchSpecAug
 
+__all__ = [
+    "FreqTimeMaskingSpecAug",
+    "TimeStretchSpecAug"
+]
